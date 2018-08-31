36904146e90ac55086ebd55797c7e1efa4bfa459
Cameo Langford <cameo.langford@c02wp0skhtd8.myob.myobcorp.net>
2018-08-31 15:12:14 +1000

    Added functionality to get player name and token before game starts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) {
         ConsolePrompt userPrompt = new ConsolePrompt();
         TicTacToeBoard board = new TicTacToeBoard();
-        Player player1 = new Player("Player 1", "X");
-        Player player2 = new Player("Player 2", "O");
+        Player player1 = getPlayerInfo(userPompt);
+
         Game ticTacToe = new Game(board, userPrompt, player1, player2);
         ticTacToe.run();
     }
+
+    private static Player getPlayerInfo(ConsolePrompt prompt){
+        prompt.print("Please enter Player name:");
+        String playerName = prompt.getInput();
+        prompt.print("Please enter Player token:");
+        String playerToken = prompt.getInput();
+        return new Player(playerName, playerToken);
+    }
+
 }
