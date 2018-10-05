c531a8053b8b2a9eb4dc69d8662b6def33668b5c
Cameo Langford <cameo.langford@myob.com>
2018-10-05 12:42:50 +1000

    Removed unused code

--- src/com/company/tictactoe/board/GameBoard.java
+++ src/com/company/tictactoe/board/GameBoard.java
@@ -3,11 +3,9 @@ package com.company.tictactoe.board;
 import com.company.tictactoe.Player;
 import com.company.tictactoe.Result;
 import com.company.tictactoe.UserMove;
-import com.company.tictactoe.WinningCombination;
 
 public abstract class GameBoard {
 
     public abstract Result addMoveToBoard(UserMove userInput, Player player);
-//    public abstract boolean tokenMatchAtPosition(WinningCombination horizontalTopRow, Player player);
     public abstract String[][] getBoard();
 }
